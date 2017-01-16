759417d8435be3cfc6cc728d5815eae55996e056
James Murphy <james.murphy19@gmail.com>
2017-01-16 17:07:09 -0600

    Different output format

--- Rubik.cpp
+++ Rubik.cpp
@@ -8,10 +8,10 @@ int main(int argc, char **argv) {
   Cube::Cube myCube(false);
   if (argc > 1) {
     std::string argString = std::string(argv[1]);
+    std::cout << "Scramble: ";
     myCube.moves(argString);
     Cross::solveCross(myCube);
     Corners::solveCorners(myCube);
-    myCube.output();
   }
   return 0;
 }
