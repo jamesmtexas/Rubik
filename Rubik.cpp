dd2193f0e2875dcf680a5fff3fc44db066e53c45
James Murphy <james.murphy19@gmail.com>
2017-01-22 11:37:50 -0600

    Allowed for standard notation input

--- Rubik.cpp
+++ Rubik.cpp
@@ -6,13 +6,16 @@
 #include "PLL.hpp"
 #include <iostream>
 
+std::string format(std::string);
+
 int main(int argc, char **argv) {
 
   Cube::Cube myCube(false);
   if (argc > 1) {
     std::string argString = std::string(argv[1]);
+    std::string scramble = format(argString);
     std::cout << "Scramble: ";
-    myCube.moves(argString);
+    myCube.moves(scramble);
     Cross::solveCross(myCube);
     Corners::solveCorners(myCube);
     Edges::solveEdges(myCube);
@@ -21,4 +24,24 @@ int main(int argc, char **argv) {
     myCube.output();
   }
   return 0;
+}
+
+std::string format(std::string s) {
+  std::string formatted;
+
+  for (int i=0; i<s.length(); ++i) {
+    if (s[i] == '\'') {
+      formatted += s[i-1];
+      formatted += s[i-1];
+    }
+    else if (s[i] == '2') {
+      formatted += s[i-1];
+    }
+    else {
+      formatted += s[i];
+    }
+  }
+
+  return formatted;
+
 }
